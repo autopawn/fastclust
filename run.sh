03cf847c07ebfd5dc4ff7458a2c23f030285c622
autopawn <franciscojacb@gmail.com>
2020-09-17 03:44:29 -0300

    Commited minor change to execution parameters.

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 make
 mkdir -p results
 
-preset1cpu2gb="--partition=general --cpus-per-task=1 --mem-per-cpu=4000M"
+preset1cpu2gb="--partition=slims --cpus-per-task=1 --mem-per-cpu=2200M"
 sbatch $preset1cpu2gb -J experiment run_experiment_job.sh
